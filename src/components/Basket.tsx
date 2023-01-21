b4ca008ef4f91dc8422ad9ad4aa7a677ae30df86
matmak13 <maksimowiczmateusz13@gmail.com>
2023-01-21 13:57:51 +0100

    Składanie zamówienia

--- src/components/Basket.tsx
+++ src/components/Basket.tsx
@@ -1,11 +1,13 @@
-import { ListItem, IconButton, ListItemText, Box, Collapse, List, Typography, Button } from "@mui/material";
+import { useState } from "react";
+
 import { DB, useAppContext } from "../App";
 import { BasketItem } from "../Types";
+
+import { ListItem, IconButton, ListItemText, Box, Collapse, List, Typography, Button, Alert } from "@mui/material";
 import { TransitionGroup } from "react-transition-group";
 import DeleteIcon from "@mui/icons-material/Delete";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
-import { useState } from "react";
 import axios from "axios";
 
 interface RenderItemOptions {
@@ -59,31 +61,57 @@ const RenderItem = ({ item, handleRemoveItem }: RenderItemOptions) => {
 const Basket = () => {
     const { basket, removeFromBasket, clearBasket, user } = useAppContext();
 
+    const [ordered, setOrdered] = useState<boolean>(false);
+    const [basketVisible, setBasketVisible] = useState<boolean>(basket.length > 0);
+
     const handleRemoveItem = (item: BasketItem) => removeFromBasket(item);
     const handleOrder = () => {
         axios.post(`http://${DB}/orders`, { order: basket, user: user });
         clearBasket();
+
+        setOrdered(true);
+
+        // Znikanie za 3 sekundy
+        setTimeout(() => {
+            setOrdered(false);
+            setBasketVisible(false);
+        }, 3000);
     };
 
-    return basket.length > 0 ? (
-        <Box className="cart" display="flex" flexDirection="column" alignItems="center">
-            <List className="cartItems" disablePadding>
-                <TransitionGroup>
-                    {basket.map((item, i) => (
-                        <Collapse key={i} sx={{ backgroundColor: i % 2 !== 0 ? "#333" : "" }}>
-                            {<RenderItem item={item} handleRemoveItem={handleRemoveItem} />}
-                        </Collapse>
-                    ))}
-                </TransitionGroup>
-            </List>
-            <Button variant="contained" sx={{ margin: 2, color: "#333", fontWeight: 500 }} color="success" onClick={handleOrder}>
-                Zamów
-            </Button>
-        </Box>
-    ) : (
-        <Typography variant="h4" align="center" className="empty" marginTop={10}>
-            Koszyk pusty
-        </Typography>
+    return (
+        <>
+            <Box position="absolute" top="0" padding={2} height="100vh" width="100vw" display="flex" flexDirection="column" alignItems="center" zIndex={-1}>
+                <Box flexGrow={1} />
+                <Collapse in={ordered} sx={{ width: "100%" }}>
+                    <Alert variant="outlined" sx={{ mb: 2 }}>
+                        Zamówienie złożone
+                    </Alert>
+                </Collapse>
+            </Box>
+
+            <Collapse in={!ordered}>
+                {basketVisible ? (
+                    <Box className="cart" display="flex" flexDirection="column" alignItems="center">
+                        <List className="cartItems" disablePadding>
+                            <TransitionGroup>
+                                {basket.map((item, i) => (
+                                    <Collapse key={i} sx={{ backgroundColor: i % 2 !== 0 ? "#333" : "" }}>
+                                        {<RenderItem item={item} handleRemoveItem={handleRemoveItem} />}
+                                    </Collapse>
+                                ))}
+                            </TransitionGroup>
+                        </List>
+                        <Button variant="contained" sx={{ margin: 2, color: "#333", fontWeight: 500 }} color="success" onClick={handleOrder}>
+                            Zamów
+                        </Button>
+                    </Box>
+                ) : (
+                    <Typography variant="h4" align="center" className="empty" marginTop={10}>
+                        Koszyk pusty
+                    </Typography>
+                )}
+            </Collapse>
+        </>
     );
 };
 
