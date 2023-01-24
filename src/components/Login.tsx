1abfc8f4d7faf1c5f75bba1a28d55968b44750fa
matmak13 <maksimowiczmateusz13@gmail.com>
2023-01-24 21:25:12 +0100

    poprawne dzialanie rejestracji

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,12 +1,9 @@
 import { DB, useAppContext } from "../App";
 import { useEffect, useState } from "react";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import FormControl from "@mui/material/FormControl";
 import Stack from "@mui/material/Stack";
 import bcrypt from "bcryptjs";
 import axios from "axios";
-import TextField from "@mui/material/TextField";
+import { TextField, Link, FormControl, Button, Typography, Box, Collapse, Alert } from "@mui/material/";
 
 const LoginForm = () => {
     const { setAdmin, setUser, setStatus, status, user } = useAppContext();
@@ -23,6 +20,10 @@ const LoginForm = () => {
             setStatus("none");
         }
     }, [setUser, setAdmin, setStatus, user]);
+
+    useEffect(() => {
+        setError(false);
+    }, [status]);
 
     const logIn = () => {
         validate();
@@ -44,67 +45,65 @@ const LoginForm = () => {
     };
 
     const registerNew = () => {
-        validate();
-        if (error) return;
+        if (!validate()) {
+            setError(true);
+            return;
+        }
+
         var hash = bcrypt.hashSync(password, 10);
         axios.post(`http://${DB}/accounts/`, { id: login, password: hash }).catch((err) => setError(true));
         if (!error) logIn();
     };
 
     const validate = () => {
-        if (login === "" || login.length < 3) {
-            setError(true);
-            return;
-        }
-        if (password === "") {
-            setError(true);
-            return;
-        }
-        if (status === "register" && password !== repetedPassword) {
-            setError(true);
-            return;
-        }
-        setError(false);
+        console.log(password, login);
+        if (login === "" || login.length < 3) return false;
+        if (password === "") return false;
+        if (status === "register" && password !== repetedPassword) return false;
     };
 
-    const errorHandle = () => {
-        if (error) {
-            if (status === "login") return <Typography>Nieprawidłowe dane logowania!</Typography>;
-            else return <Typography>Hasła się nie zgadzają lub konto już istnieje!</Typography>;
-        } else return <></>;
-    };
     const switchStatus = () => {
         if (status === "login") setStatus("register");
         else setStatus("login");
     };
 
-    const repeatPasswordField = () => {
-        if (status === "register")
-            return (
-                <FormControl fullWidth>
-                    <TextField type="password" label="Powtórz Hasło" onInput={(e: React.ChangeEvent<HTMLInputElement>) => setRepetedPassword(e.target.value)} />
-                </FormControl>
-            );
-        else return <></>;
-    };
-
     return (
         <Stack spacing={2} width={500} direction="column" justifyContent="center" alignItems="center">
+            <Box position="absolute" top="0" padding={2} height="100vh" width="100vw" display="flex" flexDirection="column" alignItems="center" zIndex={-1}>
+                <Box flexGrow={1} />
+                <Collapse in={error} sx={{ width: "100%" }}>
+                    <Alert variant="outlined" sx={{ mb: 2 }} severity={`${user === undefined ? "error" : "success"}`}>
+                        {status === "login" ? "Nieprawidłowe dane logowania!" : "Hasła się nie zgadzają lub konto już istnieje!"}
+                    </Alert>
+                </Collapse>
+            </Box>
             <Typography variant="h5" align="center">
                 {status === "login" ? "Zaloguj" : "Zarejestruj"}
             </Typography>
-            {errorHandle()}
             <FormControl fullWidth>
-                <TextField label="Login" onInput={(e: React.ChangeEvent<HTMLInputElement>) => setLogin(e.target.value)} />
+                <TextField label="Login" onInput={(e: React.ChangeEvent<HTMLInputElement>) => setLogin(e.target.value)} error={error} />
             </FormControl>
             <FormControl fullWidth>
-                <TextField type="password" label="Hasło" onInput={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} />
+                <TextField type="password" label="Hasło" onInput={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} error={error} />
             </FormControl>
-            {repeatPasswordField()}
+
+            {status === "register" && (
+                <FormControl fullWidth>
+                    <TextField
+                        type="password"
+                        label="Powtórz Hasło"
+                        onInput={(e: React.ChangeEvent<HTMLInputElement>) => setRepetedPassword(e.target.value)}
+                        error={error}
+                    />
+                </FormControl>
+            )}
+
             <Button variant="contained" onClick={status === "login" ? logIn : registerNew}>
                 {status === "login" ? "Zaloguj" : "Zarejestruj"}
             </Button>
-            <Typography onClick={switchStatus}> {status === "login" ? "Nie masz konta?" : "Masz już konto?"}</Typography>
+            <Link onClick={switchStatus} sx={{ cursor: "pointer" }}>
+                {status === "login" ? "Nie masz konta?" : "Masz już konto?"}
+            </Link>
         </Stack>
     );
 };
